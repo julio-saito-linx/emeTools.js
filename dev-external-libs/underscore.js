1ce46a96e7d48ebe67841015c69cfe0a6515c9e1
Julio Makdisse Saito <saitodisse@gmail.com>
2013-08-30 10:57:16 -0300

    to work on emeditor

--- dev-external-libs/underscore.js
+++ dev-external-libs/underscore.js
@@ -6,16 +6,12 @@
 //     For all details and documentation:
 //     http://documentcloud.github.com/underscore
 
+var _ = function(obj) { return new wrapper(obj); };
+
 (function() {
 
   // Baseline setup
   // --------------
-
-  // Establish the root object, `window` in the browser, or `global` on the server.
-  var root = this;
-
-  // Save the previous value of the `_` variable.
-  var previousUnderscore = root._;
 
   // Establish the object that gets returned to break out of a loop iteration.
   var breaker = {};
@@ -46,7 +42,6 @@
     nativeBind         = FuncProto.bind;
 
   // Create a safe reference to the Underscore object for use below.
-  var _ = function(obj) { return new wrapper(obj); };
 
   // Export the Underscore object for **Node.js**, with
   // backwards-compatibility for the old `require()` API. If we're in
@@ -57,8 +52,6 @@
       exports = module.exports = _;
     }
     exports._ = _;
-  } else {
-    root['_'] = _;
   }
 
   // Current version.
